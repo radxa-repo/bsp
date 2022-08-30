9e0a11ebdecf8958b42347becf05a1a69c5ee04a
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2022-08-30 17:46:35 +0800

    Fix rk3399 boot with mainline U-Boot & kernel

--- u-boot/.rockchip/rkbin.sh
+++ u-boot/.rockchip/rkbin.sh
@@ -1,5 +1,6 @@
 custom_source_action() {
     git_source "https://github.com/radxa/rkbin.git"
     git_am "./0001-Update-rkbin.rkbin"
+    git_am "./0002-Disable-bl32-for-rk3399.rkbin"
     source_cp "./boot_merger" "tools/"
 }
