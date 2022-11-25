06b734df0f730174fb77de7a6c15e1a47c522e02
ZHANG Yuntian <yt@radxa.com>
2022-11-25 17:27:10 +0800

    Switch back to ATF for ROCK 4

--- u-boot/.rockchip/rkbin.sh
+++ u-boot/.rockchip/rkbin.sh
@@ -1,5 +1,4 @@
 custom_source_action() {
     git_source "https://github.com/rockchip-linux/rkbin.git" b0c100f1a260d807df450019774993c761beb79d
     git_am "./0002-Disable-bl32-for-rk3399.rkbin"
-    source_cp "./boot_merger" "tools/"
 }
