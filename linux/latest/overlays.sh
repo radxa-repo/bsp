8819f1bc449a75ed5ce843feca2ba9d52c24a651
ZHANG Yuntian <yt@radxa.com>
2022-11-30 16:46:10 +0800

    Update overlays

--- linux/latest/overlays.sh
+++ linux/latest/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa/overlays.git 5b4216d0f927c16a8a1eae75e50e2cf3ee6a6eba
+    git_source https://github.com/radxa/overlays.git 557117d8a454c90e4409d168b7ce4c5bb337069a
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
