6c1b714800b4b7e2edae8fa72bfd8df03f229a4f
ZHANG Yuntian <yt@radxa.com>
2022-12-02 16:52:02 +0800

    Update radxa rkbin for RK3399

--- u-boot/.rockchip/rkbin.sh
+++ u-boot/.rockchip/rkbin.sh
@@ -1,9 +1,10 @@
 custom_source_action() {
     case $BSP_SOC in
-        rk3308|rk3328)
+        rk3308|rk3328|rk3399)
             git_source "https://github.com/radxa/rkbin.git" 9840e87723eef7c41235b89af8c049c1bcd3d133
             git_am "./0001-Update-rkbin.rkbin"
             git_am "./0002-Disable-bl32-for-rk3399.rkbin"
+            git_am "./0003-Fix-side-effect-of-broken-rkbin-history.rkbin"
             source_cp "./boot_merger" "tools/"
             ;;
         *)
