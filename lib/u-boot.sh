8629f489c96ecb40db644300475ba543f297d1ca
ZHANG Yuntian <yt@radxa.com>
2022-12-05 19:38:26 +0800

    Fix U-Boot rk3588 build

--- lib/u-boot.sh
+++ lib/u-boot.sh
@@ -128,7 +128,7 @@ rkpack_rkboot() {
     if [[ -f "$SCRIPT_DIR/.src/rkbin/RKBOOT/${BSP_TRUST_OVERRIDE^^}MINIALL.ini" ]]
     then
         $SCRIPT_DIR/.src/rkbin/tools/boot_merger "$SCRIPT_DIR/.src/rkbin/RKBOOT/${BSP_TRUST_OVERRIDE^^}MINIALL.ini"
-        mv ./${BSP_TRUST_OVERRIDE}_loader_v*.bin "$SCRIPT_DIR/.root/usr/lib/u-boot-$BSP_BOARD_OVERRIDE/rkboot.bin"
+        mv ./${BSP_TRUST_OVERRIDE}*_loader_v*.bin "$SCRIPT_DIR/.root/usr/lib/u-boot-$BSP_BOARD_OVERRIDE/rkboot.bin"
     fi
     if [[ -f "$SCRIPT_DIR/.src/rkbin/RKBOOT/${BSP_TRUST_OVERRIDE^^}MINIALL_SPINOR.ini" ]]
     then
