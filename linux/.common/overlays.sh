82965b9f671da23e3f8905245797df784657c20f
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-01-29 17:07:14 +0800

    linux: common: add rk3399 ds3231 overlay

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa/overlays.git 8b72bae4e52720a1bd311f35f724be141616e909
+    git_source https://github.com/radxa/overlays.git 5d8de27b8cc035b0298c9fe7e91f82a8e455e121
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
