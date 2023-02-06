c925ecbc9ce789352f0d6b6d8bfd302f3ce02376
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-02-06 10:23:58 +0800

    Support raspi 7-inch display for ROCK 4

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa/overlays.git 5d8de27b8cc035b0298c9fe7e91f82a8e455e121
+    git_source https://github.com/radxa/overlays.git eae18ae57b1039f35f2d0a93447a4994e018e168
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
