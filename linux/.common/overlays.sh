ef6f7f5b5fccf44b9670c417eda935813afe9611
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-02-09 20:45:43 +0800

    Improve ROCK 3C support

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa/overlays.git eae18ae57b1039f35f2d0a93447a4994e018e168
+    git_source https://github.com/radxa/overlays.git f8099f20b45b7fb8d577a5c94ec4c757a34f05a6
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
