01ba02297c46718d6a508bf2f0cbe38370cd9ce3
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-04-17 11:24:46 +0800

    u-boot: common: update ATF

---
+++ u-boot/.common/atf.sh
@@ -0,0 +1,3 @@
+custom_source_action() {
+    git_source "https://github.com/ARM-software/arm-trusted-firmware.git" "lts-v2.8.4"
+}
--- u-boot/.rockchip/atf.sh
+++
@@ -1,3 +0,0 @@
-custom_source_action() {
-    git_source "https://github.com/ARM-software/arm-trusted-firmware.git" "v2.8.0"
-}
