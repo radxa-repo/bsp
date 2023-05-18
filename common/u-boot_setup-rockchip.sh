0b2ef640b08bf555824b5d1f4fc1cfe059960d71
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-05-18 18:47:23 +0800

    u-boot: common: add maskrom_autoupdate_spinor

--- common/u-boot_setup-rockchip.sh
+++ common/u-boot_setup-rockchip.sh
@@ -71,6 +71,12 @@ maskrom_update_spinor() {
     rm /tmp/spi.img
 }
 
+maskrom_autoupdate_spinor() {
+    maskrom_spinor || true
+    maskrom_update_spinor
+    maskrom_reset
+}
+
 maskrom_dump() {
     local OUTPUT=${1:-dump.img}
 
