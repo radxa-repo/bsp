7b72e3282721db0ed808b5f2b88111dfa63c501f
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-06-19 11:27:32 +0800

    u-boot: rockchip: update rkbin

--- u-boot/.rockchip/rkbin.sh
+++ u-boot/.rockchip/rkbin.sh
@@ -1,18 +1,10 @@
 custom_source_action() {
     case $BSP_SOC in
-        rk3328|rk3399|rk3566|rk3568)
-            git_source "https://github.com/radxa/rkbin.git" cd2b28dc2c83dccdd99266bb2c43ea525bbf6c18
-            git_am "./0001-Update-rkbin.rkbin"
-            git_am "./0002-Disable-bl32-for-rk3399.rkbin"
-            git_am "./0003-Fix-side-effect-of-broken-rkbin-history.rkbin"
-            git_am "./0004-Fix-RK356X-SPI-NAND-build.rkbin"
-            source_cp "./boot_merger" "tools/"
-            ;;
         rk3308)
             git_source "https://github.com/RadxaYuntian/rkbin.git" fba57c2f676fcea54ec8317c90d04ee9ff0aff66
             ;;
         *)
-            git_source "https://gitlab.com/rk3588_linux/rk/rkbin.git" ed22a72181acedc4b725c836119ae5a04b65818f rkbin
+            git_source "https://github.com/radxa/rkbin.git" b0e55f98f0a8a5993cab9d81a40cf3c4255a34fb
             ;;
     esac
 }
