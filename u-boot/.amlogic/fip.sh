d54315046c3aefea92f68e780aba707c798d31f0
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-06-19 20:41:59 +0800

    u-boot: amlogic: update fip

--- u-boot/.amlogic/fip.sh
+++ u-boot/.amlogic/fip.sh
@@ -1,3 +1,3 @@
 custom_source_action() {
-    git_source "https://github.com/radxa/fip.git" 5b8beba2f50ce06219c4e4844101226a81aef854
+    git_source "https://github.com/radxa/fip.git" 871ca02feebdfb749b83e3f9c81823f0a7f033df
 }
