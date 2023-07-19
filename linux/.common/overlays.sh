1fa03622c477e9825174118cf05cc770c73638e5
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-07-19 20:15:52 +0800

    linux: common: update overlays

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa/overlays.git 39aec71d10078c0bb1debe9f805648c62072a221
+    git_source https://github.com/radxa/overlays.git 624ad599779f961d1fd8a9d7e1bbe51fbc9918e5
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
