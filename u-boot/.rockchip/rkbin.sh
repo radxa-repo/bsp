8fdcce02e386be405c2f566274ad5c3af6ade319
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-12-27 14:56:45 +0800

    u-boot: rockchip: update rk3308 SD NAND rkbin

--- u-boot/.rockchip/rkbin.sh
+++ u-boot/.rockchip/rkbin.sh
@@ -1,8 +1,5 @@
 custom_source_action() {
     case $BSP_SOC in
-        rk3308)
-            git_source "https://github.com/RadxaYuntian/rkbin.git" fc44f9401c127affb2a879c1e90fa89ddab505f6
-            ;;
         rk3399)
             git_source "https://github.com/radxa/rkbin.git" cd2b28dc2c83dccdd99266bb2c43ea525bbf6c18
             git_am "./0001-Update-rkbin.rkbin"
@@ -10,7 +7,7 @@ custom_source_action() {
             git_am "./0003-Fix-side-effect-of-broken-rkbin-history.rkbin"
             ;;
         *)
-            git_source "https://github.com/radxa/rkbin.git" 789dd24dacd7b6b5f57199437b6a6d31953311a3
+            git_source "https://github.com/radxa/rkbin.git" fc44f9401c127affb2a879c1e90fa89ddab505f6
             ;;
     esac
 }
