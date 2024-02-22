5a87b9973ce84b5a16ab9a976d4c76df8e23b203
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2024-02-22 15:07:27 +0800

    u-boot: atf: bump version

--- u-boot/.common/atf.sh
+++ u-boot/.common/atf.sh
@@ -1,5 +1,5 @@
 custom_source_action() {
-    git_source "https://github.com/ARM-software/arm-trusted-firmware.git" "v2.9.0"
+    git_source "https://github.com/ARM-software/arm-trusted-firmware.git" "lts-v2.10.2"
     git_patch "./enable-logging.atf"
     git_patch "./rk3328-efuse-init.atf"
 }
