0eb833daf5f86a046872061d697e237c65a73cc2
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2024-03-27 17:59:48 +0800

    linux: common: bump overlays
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa/overlays.git bbd7420526accd3e400da48878e1dbe9d510c092
+    git_source https://github.com/radxa/overlays.git 2d4109f9066251df2cf09ab2e4a7a104287f820d
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
 }
