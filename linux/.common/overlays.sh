8ee7c8aab17f886aecb560ac5ab91b1746c8e284
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2024-04-26 17:23:44 +0800

    chore: bump radxa-overlays version
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa-pkg/radxa-overlays.git 644f1317171edee1c2d6373b7a51bad3a3f96a83
+    git_source https://github.com/radxa-pkg/radxa-overlays.git 0.1.5
     cp -r $SCRIPT_DIR/.src/radxa-overlays/arch $TARGET_DIR
 }
