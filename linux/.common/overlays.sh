97d2f62dafc41916d42ae49e97ff82ca32767e90
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2024-05-23 09:55:20 +0800

    chore: update overlays
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa-pkg/radxa-overlays.git 165101555f9db6833f1b6e1748dcbf3beaf8085e
+    git_source https://github.com/radxa-pkg/radxa-overlays.git 4f8f528b21f86c7dbca8f84b6e6c5e915685cbde
     cp -r $SCRIPT_DIR/.src/radxa-overlays/arch $TARGET_DIR
 }
